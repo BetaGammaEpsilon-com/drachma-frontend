c1d762d6ea60e28d5cd4b419c3211fbb996e37fc
Robert Chatterton <70592324+robert-chatterton@users.noreply.github.com>
2022-06-01 16:32:13 -0400

    Drachma-Set up basic layout (#1)

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main>{this.props.children}</main>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Layout;
