b40bc46a6ba24c1b0b1cbbfe13b0f3b86132bfd5
robert-chatterton <robbie.chatterton@gmail.com>
2022-06-01 16:31:33 -0400

    Set up basic layout

---
+++ src/Components/Layout/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+      return(
+        <footer>
+          Footer
+        </footer>
+      );
+    }
+  }export default Footer;
