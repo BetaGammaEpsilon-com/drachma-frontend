f1e599d212eb1992b30ce562440aa5995448c05f
zohebaziz <56046187+zohebaziz@users.noreply.github.com>
2022-06-08 11:55:04 -0400

    huge restyling (#6)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors: {
+      loyalty: "#273240",
+      honor: {
+        100: "#F2CDA0",
+        200: "#F2B544",
+        300: "#F2A74B"
+      },
+      offwhite: "#F2F2F2"
+    },
     extend: {},
   },
   plugins: [],
