dd69775f4601de925921a0a5f38c3d3ac7a2c0af
zohebaziz <56046187+zohebaziz@users.noreply.github.com>
2022-06-13 12:19:01 -0400

    minor changes and adding new routes (#10)

--- src/Components/MotionComponents/MotionComponent.jsx
+++ src/Components/MotionComponents/MotionComponent.jsx
@@ -7,8 +7,9 @@ const MotionComponent = (props) => {
         <li className='grid grid-cols-3 justify-items-center py-2'>
             <p>{props.date.substring(0,10)}</p>
             <p className='lowercase'>{props.motion}</p>
-            <button className='justify-self-center outline rounded-md h-6 px-5 hover:outline-crimson hover:text-crimson'>
-                delete
+            <button 
+                className='justify-self-center outline rounded-md h-6 px-5 hover:outline-crimson hover:text-crimson'>
+                    delete
             </button>
         </li>
     );
---
+++ src/Components/TransactComponents/TransactMainScreenComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TitleComponent from "../OtherComponenets/TitleComponent";
+
+// home screen for making a new transaction
+const TransactMainScreenComponent = () => {
+
+    return (
+        <div>
+            <TitleComponent /> 
+            <form className='p-5'>
+            </form>
+        </div>
+    )
+}
+
+export default TransactMainScreenComponent;
